966b3bfead1ff878bbfed71c968284c4a7cdbf52
Roman <ramelos@yandex.ru>
2022-06-24 22:52:16 +0300

    vs6

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-import Splide from '@splidejs/splide';
+import Splide, { SLIDE } from '@splidejs/splide';
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
@@ -18,5 +18,126 @@ document.addEventListener("scroll", () => {
 });
   AOS.init();
 
- new Splide( '.splide' ).mount();
+  // new Splide(".promo-slider", {
+  //   type: "loop",
+  //   gap: "40px",
+  //   classes: {
+  //     arrow: "splide__arrow promo-slider__arrow",
+  //   },
+  //   pagination: true,
+  //   arrows: false,
+  //   // breakpoints: {
+  //   //   560: {
+  //   //     arrows: false,
+  //   //     pagination: true,
+  //   //     classes: {
+  //   //       pagination: 'splide__pagination promo-slider__pagination',
+  //   //     },
+        
+  //   //   },
+  //   // }
+  // }).mount();
+
+  new Splide(".review-slider", {
+    type: "loop",
+    gap: "40px",
+    classes: {
+      arrow: "splide__arrow review-slider__arrow",
+    },
+    pagination: false,
+    breakpoints: {
+      560: {
+        arrows: false,
+        pagination: true,
+        classes: {
+          pagination: 'splide__pagination review-slider__pagination',
+        },
+        
+      },
+    }
+  }).mount();
+
+  var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("accordion__btn_active");
+    this.classList.toggle("active");
+
+    var accordion__descript = this.nextElementSibling;
+    if (accordion__descript.style.display === "block") {
+      accordion__descript.style.display = "none";
+    } else {
+      accordion__descript.style.display = "block";
+    }
+  });
+}
+
+
+ new Splide(".autor-slider", {
+  type: "loop",
+  gap: "40px",
+  classes: {
+    arrow: "splide__arrow autor-slider__arrow",
+  },
+  pagination: false,
+  breakpoints: {
+    560: {
+      arrows: false,
+      pagination: true,
+      classes: {
+        pagination: 'splide__pagination autor-slider__pagination',
+      },
+      
+    },
+  }
+}).mount();
+
+//Promo slider
+const promoSlides = document.querySelectorAll('[data-slide-title]');
+
+let slideTitles = [];
+console.log(promoSlides);
+promoSlides.forEach(function(slide) {
+  slideTitles.push(slide.dataset.slideTitle)
+})
+
+const ptomoSlider = new Splide(".promo__slider", {
+  arrows: false,
+  drag: false,
+  autoplay: true,
+  rewind     : true,
+  rewindSpeed: 4000,
+  speed: 4000,
+  classes: {
+    pagination: 'splide__pagination promo__pagination',
+  },
+});
+
+
+ptomoSlider.on( 'pagination:mounted', function ( data ) {
+  // You can add your class to the UL element
+  data.list.classList.add( 'splide__pagination--custom' );
+  // `items` contains all dot items
+
+    data.items.forEach( function ( item ) {
+      // item.button.textContent = String( item.page + 1 );
+      item.button.textContent = ` ${slideTitles[item.page]}`;
+    } );
+
+} );
+
+ptomoSlider.mount();
+
+ //mobile menu
+  const headerBurger = document.querySelector(".header__burger");
+  const headerNav = document.querySelector(".header__nav");
+
+  headerBurger.addEventListener("click", function (event) {
+    headerBurger.classList.toggle("burger_closed");
+    headerNav.classList.toggle("header__nav_opened");
+
+  });
+
 })
